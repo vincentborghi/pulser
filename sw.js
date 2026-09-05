6f6843ccbe39b2ccafa67dc6a37ece365f14c1fa
Vincent Borghi <vincent.borghi@gmail.com>
2026-09-05 15:57:42 +0200

    Ensure footer buttons fit on a single row with flex-nowrap and compact labels

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for 100% offline usage
 // Cache version identifier
-const CACHE_NAME = "pulser-cache-v13";
+const CACHE_NAME = "pulser-cache-v14";
 
 const ASSETS_TO_CACHE = [
   "./",
